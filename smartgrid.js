2a6468e833a0b7c4898b85bf9d0598a07f96159b
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2020-04-08 06:07:11 +0300

    initial

--- smartgrid.js
+++ smartgrid.js
@@ -1,27 +1,27 @@
 module.exports = {
-    columns: 12,
-    offset: '24px',
-    container: {
-        maxWidth: '960px',
-        fields: '24px' // >= offset/2
+  columns: 12,
+  offset: '30px',
+  container: {
+    maxWidth: '1170px',
+    fields: '24px' // >= offset/2
+  },
+  breakPoints: {
+    lg: {
+        width: '1100px',
     },
-    breakPoints: {
-        // lg: {
-        //     width: '1000px',
-        // },
-        md: {
-            width: '920px',
-            fields: '15px' 
-        },
-        sm: {
-            width: '620px'
-        },
-        xs: {
-            width: '480px'
-        },
-        xxs: {
-			width: '350px',
-        }
-	},
-	detailedCalc: true 
+    md: {
+      width: '920px',
+      // fields: '15px'
+    },
+    sm: {
+      width: '620px'
+    },
+    xs: {
+      width: '480px'
+    },
+    xxs: {
+      width: '350px',
+    }
+  },
+  detailedCalc: true
 };
